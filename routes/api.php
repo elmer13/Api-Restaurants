1452651ea703c0baf3579968f64fd5de7186ac4e
Elmer Garcia <elmer_03_12@hotmail.com>
2025-07-30 15:07:31 +0200

    feat: Implement API Key authentication using Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestaurantController;
 
 Route::apiResource('restaurants', RestaurantController::class);
+
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    Route::post('register', 'AuthenticationController@register')->name('register');
+    Route::post('login', 'AuthenticationController@login')->name('login');
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('restaurants', RestaurantController::class);
+        Route::post('logout', 'AuthenticationController@logOut')->name('logout');
+    });
+});
