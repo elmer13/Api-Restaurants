4bbc76637b312fc056993c3aa417a0b23c55ed0a
Elmer Garcia <elmer_03_12@hotmail.com>
2025-07-31 02:20:38 +0200

    chore: Update app title and add controller route

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestaurantController;
 
-Route::apiResource('restaurants', RestaurantController::class);
-
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
     Route::post('register', 'AuthenticationController@register')->name('register');
     Route::post('login', 'AuthenticationController@login')->name('login');
     
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('restaurants', RestaurantController::class);
+        Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
         Route::post('logout', 'AuthenticationController@logOut')->name('logout');
     });
 });
